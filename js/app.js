917e93d3a29737a7cf0e03b9cb3bd9bce83fa74e
anampiu <anampiuandrew4@gmail.com>
2016-06-23 12:56:11 +0300

    looping function

--- js/app.js
+++ js/app.js
@@ -5,5 +5,9 @@ $(document).foundation()
 //Saving the element's i
 var orange = document.getElementById("slider");
 var blue = [];
-
+//counter i
 var i = 0;
+
+//looping the varibale through the array
+orange.src = blue[i];
+i++ //add the counter after every loop
