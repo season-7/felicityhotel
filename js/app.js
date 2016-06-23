33354b7a5558cc07821f372565d22d2e85ad0d03
anampiu <anampiuandrew4@gmail.com>
2016-06-23 12:58:02 +0300

    wrap in a function

--- js/app.js
+++ js/app.js
@@ -2,17 +2,19 @@ $(document).foundation()
 
 //Image slider javascript
 
-//Saving the element's i
-var orange = document.getElementById("slider");
-var blue = [];
-//counter i
-var i = 0;
+function slidingImage() {
+    //Saving the element's i
+    var orange = document.getElementById("slider");
+    var blue = [];
+    //counter i
+    var i = 0;
 
-//looping the varibale through the array
-orange.src = blue[i];
-i++ //add the counter after every loop
+    //looping the varibale through the array
+    orange.src = blue[i];
+    i++ //add the counter after every loop
 
-//Function to reset the counter when it gets to the end of the array
-if (i >= blue.length) {
-    i = 0;
+    //Function to reset the counter when it gets to the end of the array
+    if (i >= blue.length) {
+        i = 0;
+    }
 }
