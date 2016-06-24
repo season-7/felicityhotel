216e12333149ccb79a6476ea7936c0bcb51b230b
anampiu <anampiuandrew4@gmail.com>
2016-06-24 16:47:16 +0300

    comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 $(document).foundation()
 
 //sliding image js
+//getting the element's id and saving it
 var blue = document.getElementById("slider");
+//an array of images
 var orange = ["img/slider1.jpg", "img/slider2.jpg", "img/slider3.jpg", "img/slider4.jpg", "img/slider5.jpg", "img/slider6.jpg"];
+//counter
 var counter = 0;
-
+//function to image slider
 function slidy() {
     blue.src = orange[counter];
     counter++;
-
+    //counter reset
     if (counter >= orange.length) {
         counter = 0;
     }
