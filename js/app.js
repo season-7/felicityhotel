9c30a554523791eab3dc0946c8b53845a9c383d2
anampiu <anampiuandrew4@gmail.com>
2016-06-24 16:40:07 +0300

    test js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+//sliding image js
+var blue = document.getElementById("slider");
+var orange = ["img/slider1.jpg", "img/slider2.jpg", "img/slider3.jpg", "img/slider4.jpg", "img/slider5.jpg", "img/slider6.jpg"];
+var counter = 0;
+
+function slidy() {
+    blue.src = orange[counter];
+    counter++;
+
+    if (counter >= orange.length) {
+        counter = 0;
+    }
+}
+setInterval(slidy, 3);
